1079f6ac8fdd8058094e34f8c18e01731e2b3769
eneperezv <eneperezv@gmail.com>
2024-10-03 13:32:14 -0500

    Se incluye implementacion de envio de correo.
     Changes to be committed:
    	modified:   pom.xml
    	modified:   src/main/java/com/enp/GmailOAuth2Sender.java
    	modified:   src/main/java/com/enp/Main.java
    	modified:   target/classes/META-INF/maven/com.enp/gmail-oauth2-sender/pom.properties
    	modified:   target/classes/META-INF/maven/com.enp/gmail-oauth2-sender/pom.xml

--- src/main/java/com/enp/Main.java
+++ src/main/java/com/enp/Main.java
@@ -1,5 +1,26 @@
 package com.enp;
 
+import java.util.logging.ConsoleHandler;
+import java.util.logging.Logger;
+
+import com.google.api.client.http.HttpTransport;
+
 public class Main {
+	
+	// Habilitar el registro detallado
+    Logger logger = Logger.getLogger(HttpTransport.class.getName());
+    ConsoleHandler handler = new ConsoleHandler();
+    //handler.setLevel(Level.ALL);
+    //logger.addHandler(handler);
+    //logger.setLevel(Level.ALL);
+
+    // Ejecuta tu código aquí
+    public static void main(String[] args) {
+        try {
+            GmailOAuth2Sender.sendEmail("destinatario@correo.com", "Asunto del correo", "Contenido del correo",null);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
 }
