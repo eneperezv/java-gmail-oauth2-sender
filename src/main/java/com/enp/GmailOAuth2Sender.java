a17779e0a899409822a33bbcbb15ac2097b9368d
eneperezv <eneperezv@gmail.com>
2024-10-02 23:35:58 -0500

    Se incluye implementacion de envio de correos. Se incluye nueva dependencia.
     Changes to be committed:
    	modified:   pom.xml
    	modified:   src/main/java/com/enp/GmailOAuth2Sender.java
    	modified:   src/main/java/com/enp/Main.java
    	modified:   target/classes/META-INF/maven/com.enp/gmail-oauth2-sender/pom.properties
    	modified:   target/classes/META-INF/maven/com.enp/gmail-oauth2-sender/pom.xml

--- src/main/java/com/enp/GmailOAuth2Sender.java
+++ src/main/java/com/enp/GmailOAuth2Sender.java
@@ -1,5 +1,126 @@
 package com.enp;
 
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.util.Base64;
+import java.util.Collections;
+import java.util.List;
+import java.util.Properties;
+
+import javax.activation.DataHandler;
+import javax.activation.FileDataSource;
+import javax.mail.MessagingException;
+import javax.mail.Multipart;
+import javax.mail.Session;
+import javax.mail.internet.InternetAddress;
+import javax.mail.internet.MimeBodyPart;
+import javax.mail.internet.MimeMessage;
+import javax.mail.internet.MimeMultipart;
+
+import com.google.api.client.auth.oauth2.Credential;
+import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
+import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
+import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import com.google.api.client.json.JsonFactory;
+import com.google.api.client.json.jackson2.JacksonFactory;
+import com.google.api.client.util.store.FileDataStoreFactory;
+import com.google.api.services.gmail.Gmail;
+import com.google.api.services.gmail.GmailScopes;
+import com.google.api.services.gmail.model.Message;
+
 public class GmailOAuth2Sender {
+	
+	private static final String CREDENTIALS_FILE_PATH = "/path/to/client_secret.json"; // Reemplaza con la ruta de tu client_secret.json
+    private static final JsonFactory JSON_FACTORY = JacksonFactory.getDefaultInstance();
+    private static final String TOKENS_DIRECTORY_PATH = "tokens";
+
+    // Método para enviar un correo con adjuntos opcionales
+    public static void sendEmail(String to, String subject, String bodyText, List<File> attachments) throws Exception {
+        // Configurar Gmail API
+        NetHttpTransport HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
+        Gmail service = new Gmail.Builder(HTTP_TRANSPORT, JSON_FACTORY, getCredentials(HTTP_TRANSPORT))
+                .setApplicationName("Gmail API Java")
+                .build();
+
+        // Crear y enviar el correo
+        MimeMessage email = createEmailWithAttachments(to, "tu_correo@gmail.com", subject, bodyText, attachments);
+        sendMessage(service, "me", email);
+    }
+
+    // Métodos auxiliares
+    private static Credential getCredentials(final NetHttpTransport HTTP_TRANSPORT) throws IOException {
+        InputStream in = new FileInputStream(CREDENTIALS_FILE_PATH);
+        GoogleClientSecrets clientSecrets = GoogleClientSecrets.load(JSON_FACTORY, new InputStreamReader(in));
+        
+        GoogleAuthorizationCodeFlow flow = new GoogleAuthorizationCodeFlow.Builder(
+                HTTP_TRANSPORT, JSON_FACTORY, clientSecrets, Collections.singletonList(GmailScopes.GMAIL_SEND))
+                .setDataStoreFactory(new FileDataStoreFactory(new java.io.File(TOKENS_DIRECTORY_PATH)))
+                .setAccessType("offline")
+                .build();
+        /*
+        GoogleAuthorizationCodeFlow flow = new GoogleAuthorizationCodeFlow.Builder(
+                HTTP_TRANSPORT, JSON_FACTORY, clientSecrets, GmailScopes.GMAIL_SEND)
+                .setDataStoreFactory(new FileDataStoreFactory(new java.io.File(TOKENS_DIRECTORY_PATH)))
+                .setAccessType("offline")
+                .build();
+        */
+        return flow.loadCredential("user");
+    }
+
+    // Crear un correo con adjuntos opcionales
+    private static MimeMessage createEmailWithAttachments(String to, String from, String subject, String bodyText, List<File> attachments) throws MessagingException, IOException {
+        Properties props = new Properties();
+        Session session = Session.getDefaultInstance(props, null);
+
+        MimeMessage email = new MimeMessage(session);
+        email.setFrom(new InternetAddress(from));
+        email.addRecipient(javax.mail.Message.RecipientType.TO, new InternetAddress(to));
+        email.setSubject(subject);
+
+        // Crear el cuerpo del mensaje
+        MimeBodyPart textPart = new MimeBodyPart();
+        textPart.setText(bodyText);
+
+        Multipart multipart = new MimeMultipart();
+        multipart.addBodyPart(textPart);
+
+        // Agregar los adjuntos (si existen)
+        if (attachments != null && !attachments.isEmpty()) {
+            for (File file : attachments) {
+                MimeBodyPart attachmentPart = new MimeBodyPart();
+                FileDataSource source = new FileDataSource(file);
+                attachmentPart.setDataHandler(new DataHandler(source));
+                attachmentPart.setFileName(file.getName());
+                multipart.addBodyPart(attachmentPart);
+            }
+        }
+
+        // Agregar el contenido al correo
+        email.setContent(multipart);
+        return email;
+    }
+
+    // Método para enviar el correo
+    private static void sendMessage(Gmail service, String userId, MimeMessage emailContent)
+            throws MessagingException, IOException {
+        Message message = createMessageWithEmail(emailContent);
+        service.users().messages().send(userId, message).execute();
+    }
+
+    private static Message createMessageWithEmail(MimeMessage emailContent)
+            throws MessagingException, IOException {
+        ByteArrayOutputStream buffer = new ByteArrayOutputStream();
+        emailContent.writeTo(buffer);
+        byte[] rawMessageBytes = buffer.toByteArray();
+        String encodedEmail = Base64.getUrlEncoder().encodeToString(rawMessageBytes);
+        Message message = new Message();
+        message.setRaw(encodedEmail);
+        return message;
+    }
 
 }
--- src/main/java/com/enp/Main.java
+++ src/main/java/com/enp/Main.java
@@ -1,5 +1,13 @@
 package com.enp;
 
 public class Main {
+	
+	public static void main(String[] args) {
+        try {
+            GmailOAuth2Sender.sendEmail("destinatario@correo.com", "Asunto del correo", "Contenido del correo", null);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 
 }
